d48e995a932c4b9efae8da2c7b810c012eab0e15
Juriba <Juribaalsiari@gmail.com>
2024-08-12 01:30:06 +0300

    wip

---
+++ src/main/java/com/juriba/tracker/common/application/UseCase.java
@@ -0,0 +1,19 @@
+package com.juriba.tracker.common.application;
+
+
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Service
+@Validated
+public @interface UseCase {
+}
--- src/main/java/com/juriba/tracker/user/domain/Role.java
+++ src/main/java/com/juriba/tracker/user/domain/Role.java
@@ -5,20 +5,23 @@ import com.juriba.tracker.user.domain.event.RoleCreatedEvent;
 import com.juriba.tracker.user.domain.event.RoleNameChangedEvent;
 import jakarta.persistence.Entity;
 import jakarta.persistence.*;
-import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Table(name = "roles")
 @EqualsAndHashCode(callSuper = true)
-@Builder
 @Getter
 @NoArgsConstructor
 public class Role extends AggregateRoot {
     @Column(unique = true, nullable = false)
     private String name;
+    @ManyToMany(mappedBy = "roles")
+    private final Set<User> users = new HashSet<>();
 
     public Role(String name) {
         this.name = name;
